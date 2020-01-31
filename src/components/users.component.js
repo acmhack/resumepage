5e5225ff17fcc08e93e4420ec0c31c92a6a7f131
Colton Jacobson <30200979+ColtonJacobson23@users.noreply.github.com>
2020-01-30 18:55:31 -0600

    Another change

--- src/components/users.component.js
+++ src/components/users.component.js
@@ -12,7 +12,7 @@ var data = [];
 function viewResume(resume) {
   var pdfAsDataUri = "data:application/pdf;base64," + resume;
   var win = window.open();
-  win.document.write('<iframe src="' + pdfAsDataUri + '" frameborder="0" style="border:0; top:0px; left:0px; bottom:0px; right:0px; width:100%; height:100%;" allowfullscreen></iframe>);
+  win.document.write('<iframe src="' + pdfAsDataUri + '" frameborder="0" style="border:0; top:0px; left:0px; bottom:0px; right:0px; width:100%; height:100%;" allowfullscreen></iframe>');
 }
 
 
