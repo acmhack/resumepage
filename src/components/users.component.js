6166349696efc097bdc838ce0255abbdbeb227df
Colton Jacobson <30200979+ColtonJacobson23@users.noreply.github.com>
2020-01-30 18:35:56 -0600

    Fix Open

--- src/components/users.component.js
+++ src/components/users.component.js
@@ -5,13 +5,14 @@ import { Link } from 'react-router-dom';
 import axios from 'axios';
 import { Table, Input, Button, Icon, Switch, Radio, Form, Divider } from 'antd';
 import Highlighter from 'react-highlight-words';
-//import './security.js';
+import './security.js';
 
 var data = [];
 
 function viewResume(resume) {
   var pdfAsDataUri = "data:application/pdf;base64," + resume;
-  window.open(pdfAsDataUri);
+  var win = window.open();
+  win.document.write('<iframe src="' + pdfAsDataUri + '" frameborder="0" style="border:0; top:0px; left:0px; bottom:0px; right:0px; width:100%; height:100%;" allowfullscreen></iframe>);
 }
 
 
