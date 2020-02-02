ad1aabece54f2bd74cf3514fa3bb90056ad357f4
Colton Jacobson <30200979+ColtonJacobson23@users.noreply.github.com>
2020-02-02 17:28:39 -0600

    Pulls confirmed users now

--- backend/routes/users.js
+++ backend/routes/users.js
@@ -2,7 +2,7 @@ const router = require('express').Router();
 let User = require('../models/user.model');
 
 router.route('/').get((req, res) => {
-  User.find({"status.checkedIn": true })
+  User.find({"status.confirmed": true })
 	.then(users => res.json(users))
 	.catch(err => res.status(400).json('Error: ' + err));
 });
