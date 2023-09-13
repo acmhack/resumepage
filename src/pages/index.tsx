d2174c2e233184bb0e138eadc60cdc466e585752
EricDuong <ericlamduong@gmail.com>
2023-09-13 18:26:57 -0500

    initialize usercard display with inline styling (temp)

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -4,37 +4,24 @@ import Image from 'next/image'
 import styles from '../styles/Home.module.css'
 import { Card, CardContent, CardActions, Button } from '@mui/material'
 import UserCard from '../components/usercard'
+import { JSX, useEffect } from 'react'
 
 
+const testArray: any = [];
+
+const addCard = ({name, projectlink, resumelink}: any) => {
+  for (let index = 0; index < 5; index++) {
+    testArray.push()
+  }
+}
 
 const Home: NextPage = () => {
+
+
   return (
     <div style={{height: '100%'}}>
       <h1>PICKHACKS RESUME PAGE!! :)</h1>
-      <div style={{display: 'flex', gap: 30, flexFlow: 'wrap', height: '80vh', width: '100vw', padding: 100, overflow: 'scroll'}}>
-        <UserCard name='gamer' projectlink='null' resumelink='null' />
-        <UserCard name='gamer' projectlink='null' resumelink='null' />
-        <UserCard name='gamer' projectlink='null' resumelink='null' />
-        <UserCard name='gamer' projectlink='null' resumelink='null' />
-        <UserCard name='gamer' projectlink='null' resumelink='null' />
-        <UserCard name='gamer' projectlink='null' resumelink='null' />
-        <UserCard name='gamer' projectlink='null' resumelink='null' />
-        <UserCard name='gamer' projectlink='null' resumelink='null' />
-        <UserCard name='gamer' projectlink='null' resumelink='null' />
-        <UserCard name='gamer' projectlink='null' resumelink='null' />
-        <UserCard name='gamer' projectlink='null' resumelink='null' />
-        <UserCard name='gamer' projectlink='null' resumelink='null' />
-        <UserCard name='gamer' projectlink='null' resumelink='null' />
-        <UserCard name='gamer' projectlink='null' resumelink='null' />
-        <UserCard name='gamer' projectlink='null' resumelink='null' />
-        <UserCard name='gamer' projectlink='null' resumelink='null' />
-        <UserCard name='gamer' projectlink='null' resumelink='null' />
-        <UserCard name='gamer' projectlink='null' resumelink='null' />
-        <UserCard name='gamer' projectlink='null' resumelink='null' />
-        <UserCard name='gamer' projectlink='null' resumelink='null' />
-        <UserCard name='gamer' projectlink='null' resumelink='null' />
-        <UserCard name='gamer' projectlink='null' resumelink='null' />
-        <UserCard name='gamer' projectlink='null' resumelink='null' />
+      <div id='top' style={{display: 'flex', gap: 40, flexFlow: 'wrap', height: '80vh', width: '90vw', padding: 100, overflow: 'scroll', alignContent: 'flex-start', borderStyle: 'solid'}}>
         <UserCard name='gamer' projectlink='null' resumelink='null' />
         <UserCard name='gamer' projectlink='null' resumelink='null' />
         <UserCard name='gamer' projectlink='null' resumelink='null' />
