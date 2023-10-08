0e37c6a4eaad1cc78655486fe29bac5a859ccc2b
ewbyf <107148516+ewbyf@users.noreply.github.com>
2023-10-08 17:01:42 -0500

    styling changes

--- src/components/layout.tsx
+++ src/components/layout.tsx
@@ -1,270 +1,62 @@
-
-import styles from '../styles/components/Layout.module.css'
+import styles from '../styles/components/Layout.module.css';
 import * as React from 'react';
-import { styled, alpha } from '@mui/material/styles';
-import AppBar from '@mui/material/AppBar';
-import Box from '@mui/material/Box';
-import Toolbar from '@mui/material/Toolbar';
-import IconButton from '@mui/material/IconButton';
-import Typography from '@mui/material/Typography';
-import InputBase from '@mui/material/InputBase';
-import Badge from '@mui/material/Badge';
-import MenuItem from '@mui/material/MenuItem';
-import Menu from '@mui/material/Menu';
-import MenuIcon from '@mui/icons-material/Menu';
 import SearchIcon from '@mui/icons-material/Search';
-import MoreIcon from '@mui/icons-material/MoreVert';
-import Button from '@mui/material/Button';
-
-const pages = ["Sort"];
-const pages1 = ["Filter"];
-const settings = ["Ascending", "Descending"];
-const settings2 = ["Categories", "Year"];
-
-const Search = styled('div')(({ theme }) => ({
-    position: 'relative',
-    borderRadius: theme.shape.borderRadius,
-    backgroundColor: alpha(theme.palette.common.white, 0.15),
-    '&:hover': {
-    backgroundColor: alpha(theme.palette.common.white, 0.25),
-    },
-    marginRight: theme.spacing(2),
-    marginLeft: 0,
-    width: '100%',
-    [theme.breakpoints.up('sm')]: {
-    marginLeft: theme.spacing(2),
-    width: 450,
-    },
-}));
-
-const SearchIconWrapper = styled('div')(({ theme }) => ({
-    padding: theme.spacing(0, 2),
-    height: '100%',
-    position: 'absolute',
-    pointerEvents: 'none',
-    display: 'flex',
-    alignItems: 'center',
-    justifyContent: 'center',
-    
-    
-}));
-
-const StyledInputBase = styled(InputBase)(({ theme }) => ({
-    color: 'inherit',
-    '& .MuiInputBase-input': {
-    padding: theme.spacing(1, 1, 1, 0),
-      // vertical padding + font size from searchIcon
-    paddingLeft: `calc(1em + ${theme.spacing(4)})`,
-    transition: theme.transitions.create('width'),
-    width: '100%',
-    [theme.breakpoints.up('md')]: {
-        width: '20ch',
-        },
-    },
-}));
-export function PrimarySearchAppBar() {
-    const [anchorEl, setAnchorEl] = React.useState<null | HTMLElement>(null);
-    const [mobileMoreAnchorEl, setMobileMoreAnchorEl] =
-      React.useState<null | HTMLElement>(null);
-  
-    const isMenuOpen = Boolean(anchorEl);
-    const isMobileMenuOpen = Boolean(mobileMoreAnchorEl);
-  
-    const handleProfileMenuOpen = (event: React.MouseEvent<HTMLElement>) => {
-      setAnchorEl(event.currentTarget);
-    };
-  
-    const handleMobileMenuClose = () => {
-      setMobileMoreAnchorEl(null);
-    };
-  
-    const handleMenuClose = () => {
-      setAnchorEl(null);
-      handleMobileMenuClose();
-    };
-  
-    const handleMobileMenuOpen = (event: React.MouseEvent<HTMLElement>) => {
-      setMobileMoreAnchorEl(event.currentTarget);
-    };
-  
-    const menuId = 'primary-search-account-menu';
-    const renderMenu = (
-      <Menu
-        anchorEl={anchorEl}
-        anchorOrigin={{
-          vertical: 'top',
-          horizontal: 'right',
-        }}
-        id={menuId}
-        keepMounted
-        transformOrigin={{
-          vertical: 'top',
-          horizontal: 'right',
-        }}
-        open={isMenuOpen}
-        onClose={handleMenuClose}
-      >
-      </Menu>
-    );
-}   
-
+import { FormControl, FormControlLabel, FormGroup, InputLabel, Select, MenuItem, Checkbox } from '@mui/material';
+import { useState } from 'react';
 
 const Layout: React.FC<React.PropsWithChildren> = ({ children }) => {
-    const [anchorElNav, setAnchorElNav] = React.useState<null | HTMLElement>(
-      null
-    );
-    const [anchorElUser, setAnchorElUser] = React.useState<null | HTMLElement>(
-      null
-    );
+    const [sort, setSort] = useState('Featured');
 
-    const handleOpenNavMenu = (event: React.MouseEvent<HTMLElement>) => {
-      setAnchorElNav(event.currentTarget);
-    };
-    const handleOpenUserMenu = (event: React.MouseEvent<HTMLElement>) => {
-      setAnchorElUser(event.currentTarget);
-    };
-
-    const handleCloseNavMenu = () => {
-      setAnchorElNav(null);
-    };
-
-    const handleCloseUserMenu = () => {
-      setAnchorElUser(null);
-    };
-    const [anchorEl, setAnchorEl] = React.useState<null | HTMLElement>(null);
-    const [mobileMoreAnchorEl, setMobileMoreAnchorEl] =
-      React.useState<null | HTMLElement>(null);
-  
-    const isMenuOpen = Boolean(anchorEl);
-    const isMobileMenuOpen = Boolean(mobileMoreAnchorEl);
-  
-    const handleProfileMenuOpen = (event: React.MouseEvent<HTMLElement>) => {
-      setAnchorEl(event.currentTarget);
-    };
-  
-    const handleMobileMenuClose = () => {
-      setMobileMoreAnchorEl(null);
-    };
-  
-    const handleMenuClose = () => {
-      setAnchorEl(null);
-      handleMobileMenuClose();
-    };
-  
-    const handleMobileMenuOpen = (event: React.MouseEvent<HTMLElement>) => {
-      setMobileMoreAnchorEl(event.currentTarget);
-    };
-  
-    const menuId = 'primary-search-account-menu';
-    const renderMenu = (
-      <Menu
-        anchorEl={anchorEl}
-        anchorOrigin={{
-          vertical: 'top',
-          horizontal: 'right',
-        }}
-        id={menuId}
-        keepMounted
-        transformOrigin={{
-          vertical: 'top',
-          horizontal: 'right',
-        }}
-        open={isMenuOpen}
-        onClose={handleMenuClose}
-      >
-      </Menu>
-    );
-    const mobileMenuId = 'primary-search-account-menu-mobile';
-    const renderMobileMenu = (
-    <Menu
-      anchorEl={mobileMoreAnchorEl}
-      anchorOrigin={{
-        vertical: 'top',
-        horizontal: 'right',
-      }}
-      id={mobileMenuId}
-      keepMounted
-      transformOrigin={{
-        vertical: 'top',
-        horizontal: 'right',
-      }}
-      open={isMobileMenuOpen}
-      onClose={handleMobileMenuClose}
-    >
-    </Menu>
-  );  
     return (
-    <div className={styles.container}>
-    <Box sx={{ flexGrow: 2 }}>
-        <AppBar position="static">
-        <Toolbar>
-            <Search>
-            <SearchIconWrapper>
-                <SearchIcon />
-            </SearchIconWrapper>
-            <StyledInputBase
-                placeholder="Search…"
-                inputProps={{ 'aria-label': 'search' }}
-            />
-            </Search>
-            <Box sx={{ flexGrow: 1, display: { xs: 'none', md: 'flex' } }}>
-            {pages.map((page) => (
-              <Button
-                key={page}
-                onClick={handleOpenUserMenu}
-                sx={{ my: 2, color: 'white', display: 'block' }}
-              >
-                {page}
-              </Button> 
-            ))}
-            {pages1.map((page) => (
-              <Button
-                key={page}
-                onClick={handleOpenUserMenu}
-                sx={{ my: 2, color: 'white', display: 'block' }}
-              >
-                {page}
-              </Button> 
-            ))}
-          </Box>
-          <Box sx={{ flexGrow: 0 }}>
-            <Menu
-              sx={{ mt: '45px' }}
-              id="menu-appbar"
-              anchorEl={anchorElUser}
-              anchorOrigin={{
-                vertical: 'top',
-                horizontal: 'right',
-              }}
-              keepMounted
-              transformOrigin={{
-                vertical: 'top',
-                horizontal: 'right',
-              }}
-              open={Boolean(anchorElUser)}
-              onClose={handleCloseUserMenu}
-            >
-              {settings.map((setting) => (
-                <MenuItem key={setting} onClick={handleCloseUserMenu}>
-                  <Typography textAlign="center">{setting}</Typography>
-                </MenuItem>
-              ))}
-               {settings2.map((setting) => (
-                <MenuItem key={setting} onClick={handleCloseUserMenu}>
-                  <Typography textAlign="center">{setting}</Typography>
-                </MenuItem>
-              ))}
-        
-            </Menu>
-          </Box>
-        </Toolbar>
-        </AppBar>
-        {renderMobileMenu}
-        {renderMenu}
-    </Box>
-    
-        {children}
+        <div className={styles.container}>
+            <div className={styles.sidebar}>
+                <div className={styles.search}>
+                    <SearchIcon
+                        sx={{
+                            position: 'absolute',
+                            left: 18,
+                            zIndex: 99
+                        }}
+                    />
+                    <input type="text" className={styles.searchbar} placeholder="Search..." />
+                </div>
+                {/* TODO: Clear filters */}
+                <div className={styles.filter}>
+                    <p className={styles.title}>Grad Year</p>
+                    <FormGroup>
+                        <FormControlLabel control={<Checkbox />} label="2023" />
+                        <FormControlLabel control={<Checkbox />} label="2024" />
+                        <FormControlLabel control={<Checkbox />} label="2025" />
+                        <FormControlLabel control={<Checkbox />} label="2026" />
+                    </FormGroup>
+                </div>
+                <div className={styles.line}></div>
+                <div className={styles.filter}>
+                    <p className={styles.title}>Categories</p>
+                    <FormGroup>
+                        <FormControlLabel control={<Checkbox />} label="Overall" />
+                        <FormControlLabel control={<Checkbox />} label="Beginner" />
+                        <FormControlLabel control={<Checkbox />} label="Solo" />
+                    </FormGroup>
+                </div>
+                <div className={styles.line}></div>
+                <div className={styles.filter}>
+                    <FormControl fullWidth>
+                        <InputLabel>Sort By</InputLabel>
+                        <Select
+                            value={sort}
+                            label="Sort By"
+                            onChange={(e) => setSort(e.target.value)}
+                        >
+                            <MenuItem value={'Featured'}>Featured</MenuItem>
+                            <MenuItem value={'Ascending'}>Ascending</MenuItem>
+                            <MenuItem value={'Descending'}>Descending</MenuItem>
+                        </Select>
+                    </FormControl>
+                </div>
+            </div>
+            {children}
         </div>
-        );
-  } 
+    );
+};
 export default Layout;
